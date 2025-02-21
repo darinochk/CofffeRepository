f33b3aff2b6c717d009f530aa1c716ef87bc9225
darinchik <darinochk/ProgLab3>
2025-02-21 22:58:11 +0300

    feat: initial commit

---
+++ src/main/java/org/example/coffeservice/CoffeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.coffeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeServiceApplication.class, args);
+    }
+
+}
